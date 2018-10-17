e6faad8634624710f06e1e45e64a90807f96a5cd
Rob Pearson <rob.pearson@me.com>
2018-10-18 09:31:00 +1000

    Updated initial hard coded data for the Product/Pet service.

--- OctopusSamples.ProductService/Controllers/ProductsController.cs
+++ OctopusSamples.ProductService/Controllers/ProductsController.cs
@@ -17,9 +17,38 @@ namespace OctopusSamples.ProductService.Controllers
             {
                 Id = 1,
                 Name = "Dog",
-                Description = "Black and white border collie",
-                ImageUrl = "https://"
-            }
+                Description = "Black and white Border Collie puppy",
+                ImageUrl = "https://s3.amazonaws.com/i.octopus.com/videos/Dog.png",
+                Price = 100.0,
+                InStock = true
+            },
+            new ProductDetail
+            {
+                Id = 2,
+                Name = "Cat",
+                Description = "Friendly kitten",
+                ImageUrl = "https://s3.amazonaws.com/i.octopus.com/videos/Cat.png",
+                Price = 75.0,
+                InStock = false
+            }, 
+            new ProductDetail
+            {
+                Id = 4,
+                Name = "Fish",
+                Description = "Goldie the goldfish",
+                ImageUrl = "https://s3.amazonaws.com/i.octopus.com/videos/Fish.png",
+                Price = 15.0,
+                InStock = true
+            },
+            new ProductDetail
+            {
+                Id = 4,
+                Name = "Octopus",
+                Description = "The amazing wonder of the ocean",
+                ImageUrl = "https://s3.amazonaws.com/i.octopus.com/videos/Octopus.png",
+                Price = 250.0,
+                InStock = true
+            },
         };
         
         [HttpGet]
--- OctopusSamples.ProductService/Models/ProductDetail.cs
+++ OctopusSamples.ProductService/Models/ProductDetail.cs
@@ -7,5 +7,6 @@ namespace OctopusSamples.ProductService.Models
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public bool InStock { get; set; }
+        public double Price { get; set; }
     }
 }
