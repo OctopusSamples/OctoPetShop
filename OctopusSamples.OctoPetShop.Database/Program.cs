95b57d91f6e85d6206b9ef232cc644eb710be1c6
twerthi <twerthi@hotmail.com>
2020-05-27 14:56:26 -0700

    Still fixing spacing

--- OctopusSamples.OctoPetShop.Database/Program.cs
+++ OctopusSamples.OctoPetShop.Database/Program.cs
@@ -10,43 +10,43 @@ namespace OctopusSamples.OctoPetShopDatabase
     {
         static int Main(string[] args)
         {
-			var retryCount = 0;
-			var environmentVariableConnectionString = Environment.GetEnvironmentVariable("DbUpConnectionString");
+            var retryCount = 0;
+            var environmentVariableConnectionString = Environment.GetEnvironmentVariable("DbUpConnectionString");
             var connectionString = environmentVariableConnectionString == null ? args.FirstOrDefault() ?? "Server=(local)\\SqlExpress; Database=ops; Trusted_connection=true" : environmentVariableConnectionString;
 
 
-			// retry three times
-			while (true)
-			{
-				try
-				{
-					EnsureDatabase.For.SqlDatabase(connectionString);
-					break;
-				}
-				catch (System.Data.SqlClient.SqlException)
-				{
-					// check type
-					if (retryCount < 3)
-					{
-						// Display
-						Console.WriteLine("Connection error occured, waiting 3 seconds then trying again.");
-						System.Threading.Thread.Sleep(3000);
-						retryCount += 1;
-					}
-					else
-					{
-						// rethrow
-						throw;
-					}
-				}
-			}
-			
+            // retry three times
+            while (true)
+            {
+                try
+                {
+                    EnsureDatabase.For.SqlDatabase(connectionString);
+                    break;
+                }
+                catch (System.Data.SqlClient.SqlException)
+                {
+                    // check type
+                    if (retryCount < 3)
+                    {
+                        // Display
+                        Console.WriteLine("Connection error occured, waiting 3 seconds then trying again.");
+                        System.Threading.Thread.Sleep(3000);
+                        retryCount += 1;
+                    }
+                    else
+                    {
+                        // rethrow
+                        throw;
+                    }
+                }
+            }
+
             var upgrader =
-                DeployChanges.To
-                    .SqlDatabase(connectionString)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
+            DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
 
             var result = upgrader.PerformUpgrade();
 
