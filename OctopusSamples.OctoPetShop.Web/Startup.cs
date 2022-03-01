534d10c8b637953c3e6d2cdd3ed828c093f83f9d
Peter Gallagher <pete@pjgcreations.co.uk>
2022-03-01 11:42:30 +0000

    Removed redundant package. Updated Startup.cs

--- OctopusSamples.OctoPetShop.Web/Startup.cs
+++ OctopusSamples.OctoPetShop.Web/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using OctopusSamples.OctoPetShop.Controllers.Client;
 
 namespace OctopusSamples.OctoPetShop
@@ -26,14 +27,24 @@ namespace OctopusSamples.OctoPetShop
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
             services.Configure<AppSettings>(Configuration);
             services.AddScoped<IProductClient, ProductClient>();
+
+            services.AddMvcCore(options =>
+            {
+                //options.RequireHttpsPermanent = true; //does not affect API requests
+                options.RespectBrowserAcceptHeader = true; //false by default
+            })
+            .AddApiExplorer()
+            .AddFormatterMappings()
+            .AddNewtonsoftJson()
+            .AddCacheTagHelper()
+            .AddDataAnnotations();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -49,11 +60,12 @@ namespace OctopusSamples.OctoPetShop
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
